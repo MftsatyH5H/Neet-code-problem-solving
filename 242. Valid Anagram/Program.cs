eca59fa98f2cae30690ba3249595e35c2903dd38
Mohamed Alaa <momoalola681@gmail.com>
2023-11-23 14:53:38 -0800

    Solved Valid Anagram Problem

---
+++ 242. Valid Anagram/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        public bool IsAnagram(string s, string t) {
+            for( int i = 0 ; i<s.Length ; i++ ){
+                for ( int j =0 ; j<t.Length ; j++ ){
+                    if (s[i] == t[j]){
+                        break;
+                    }
+                    else if (j == t.Length-1){
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            string string1 = "hellozzw";
+            string string2 = "elloh";
+
+        }
+    }
+}
